f657ee24958e9ae6f594d06325365e544ef3cdb4
felipedafig <felipedafig@gmail.com>
2025-11-20 23:35:17 +0100

    agents properties view

--- Shared/DTOs/obj/Debug/net10.0/DTOs.AssemblyInfo.cs
+++ Shared/DTOs/obj/Debug/net10.0/DTOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7f8cd4173eb12f8251a3aafef224aa82ac1a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e377821676e1daf78454e500f2a664c39b25505")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
