9724390ae088bea08d90d1d1814cc79174ccd5bc
felipedafig <felipedafig@gmail.com>
2025-11-23 19:42:47 +0100

    DataTierServerImpl tests set up (TDD)

--- Shared/DTOs/obj/Debug/net10.0/DTOs.AssemblyInfo.cs
+++ Shared/DTOs/obj/Debug/net10.0/DTOs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef11812592b323f7396c130a3f7f53830c523505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd3f3026d98b039048f37d794c17183b5b362e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
