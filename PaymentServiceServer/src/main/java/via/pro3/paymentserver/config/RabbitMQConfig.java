4ddc9a1aff9bab0ffba75ceb381fdac365005f99
felipedafig <felipedafig@gmail.com>
2025-12-03 15:07:54 +0100

    adding attributes in payment proto

--- PaymentServiceServer/src/main/java/via/pro3/paymentserver/config/RabbitMQConfig.java
+++ PaymentServiceServer/src/main/java/via/pro3/paymentserver/config/RabbitMQConfig.java
@@ -1,11 +1,14 @@
 package via.pro3.paymentserver.config;
 
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class RabbitMQConfig {
 
     public static final String payment_validated_exchange = "payment.validated";
@@ -15,4 +18,11 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter(){ return new Jackson2JsonMessageConverter();}
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
 }
